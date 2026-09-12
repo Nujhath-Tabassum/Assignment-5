978086ac14b9e4b22b3d9b217db1ed1d8d046461
Nujhath Tabassum <snujhathtabassum@gmail.com>
2026-09-12 12:42:25 +0600

    Remove from Stack Toast added

--- src/components/TechnologiesSection/Technology.tsx
+++ src/components/TechnologiesSection/Technology.tsx
@@ -2,6 +2,7 @@
 import { use, useState } from "react";
 import type { Technology as TechnologyType } from "../Types/type";
 import TechonologyCard from "./TechnologyCard";
+import { toast } from "react-toastify";
 
 interface TechnologyProps {
     TechnologyPromise: Promise<TechnologyType[]>
@@ -18,6 +19,15 @@ function Technology({ TechnologyPromise }: TechnologyProps) {
 
     setStack([...stack, technology])
 }
+const handleRemoveFromStack = (technology: TechnologyType) => {
+    setStack((prevStack) =>
+        prevStack.filter(
+            (item) => item.name !== technology.name
+        )
+    );
+
+    toast.info("Removed from Stack");
+};
     return (
         <section className="px-20 py-16">
 
@@ -53,75 +63,34 @@ function Technology({ TechnologyPromise }: TechnologyProps) {
                          <h2 className="text-lg font-semibold text-gray-900"> Your Stack</h2>
 
                             <p className="text-xs text-gray-400 mt-1">
-                                {stack.length === 0
-                                    ? "No technologies selected yet."
-                                    : `${stack.length} Technology Selected${stack.length > 1 ? "s" : ""}`
-                                }
-                            </p>
+                                {stack.length === 0? "No technologies selected yet." : `${stack.length} Technology Selected${stack.length > 1 ? "s" : ""}`}</p>
 
 
                             {stack.length === 0 ? (
 
                                 <div className="m-4 h-10 border border-dashed border-gray-300 rounded-xl flex items-center justify-center">
-                                    <p className="text-xs text-gray-400">
-                                        Your stack is empty.
-                                    </p>
+                                    <p className="text-xs text-gray-400">Your stack is empty. </p>
                                 </div>
 
                             ) : (
 
-                                <div className="mt-4 space-y-2">
+                                <div className="mt-4 space-y-2">{stack.map((technology) => (
 
-                                    {stack.map((technology) => (
+                                        <div key={technology.name}className="border border-gray-200 rounded-lg p-3 flex items-center gap-3" >
 
-                                        <div
-                                            key={technology.name}
-                                            className="border border-gray-200 rounded-lg p-3 flex items-center gap-3"
-                                        >
-
-                                            <img
-                                                src={technology.logo}
-                                                alt={technology.name}
-                                                className="w-9 h-9 object-contain"
-                                            />
+                                            <img src={technology.logo}alt={technology.name} className="w-9 h-9 object-contain"/>
 
                                             <div>
-                                                <h3 className="text-xs font-semibold text-gray-900">
-                                                    {technology.name}
-                                                </h3>
+                                                <h3 className="text-xs font-semibold text-gray-900"> {technology.name}</h3>
 
-                                                <p className="text-[10px] text-gray-400">
-                                                    {technology.category}
-                                                </p>
+                                                <p className="text-[10px] text-gray-400"> {technology.category} </p>
                                             </div>
 
 
-                                            <button
-                                                onClick={() =>
-                                                    setStack(
-                                                        stack.filter(
-                                                            (item) =>
-                                                                item.name !== technology.name
-                                                        )
-                                                    )
-                                                }
-                                                className="ml-auto text-xl font-light text-gray-400 hover:text-gray-600"
-                                            >
-                                                ×
-                                            </button>
+                                            <button onClick={() => handleRemoveFromStack(technology)} className="ml-auto text-xl font-light text-gray-400 hover:text-gray-600"> ×</button>
 
-                                        </div>
-
-                                    ))}
-
-
-                                   
-                                    <button
-                                        onClick={() => setStack([])}
-                                        className="w-full mt-10 h-8 border border-red-300 rounded-lg text-sm font-medium text-red-500 hover:bg-red-50 transition"
-                                    >
-                                        Remove All
-                                    </button>
+                                        </div>))}
+                                    <button onClick={() => setStack([])}className="w-full mt-10 h-8 border border-red-300 rounded-lg text-sm font-medium text-red-500 hover:bg-red-50 transition">Remove All</button>
 
                                 </div>
 
